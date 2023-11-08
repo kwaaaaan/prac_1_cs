30e2851bd9cda3aca2e00194d9ca5acfec5ac8a1
KWAN <kjg9406@gmail.com>
2023-11-08 14:43:43 +0900

    Add project files.

---
+++ prac_1_cs/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_1_cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            string value = textBox_input.Text;
+        }
+    }
+}
